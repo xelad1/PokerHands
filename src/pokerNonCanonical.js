0f2645f77be2002ea2391057a872ab9713bc9987
xelad1 <xelad1@gmail.com>
2015-03-24 17:06:52 -0500

    Added helper functions to noncanonical

--- src/pokerNonCanonical.js
+++ src/pokerNonCanonical.js
@@ -3,5 +3,30 @@ var poker = {
   // See the specs for the required formats.
   labelHand: function(hand) {
 
+  	var splitHand = hand.split(" ");
+
+  	var traverseHand = function(checker) {
+
+  		for(var i = 0; i < splitHand.length; i ++) {
+
+  		}
+  	}
+
+  	var highCard = function(hand) {
+
+  	}
+
+  	var pair = function(hand) {
+
+  	}
+
+  	var twoPair = function(hand) {
+
+  	}
+
+  	var threeOfKind = function(hand) {
+  		
+  	}
+
   }
 };
